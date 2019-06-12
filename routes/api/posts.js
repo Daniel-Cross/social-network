19432ba631f8f10b7bc004973dd68a80374c7cca
Daniel Cross <crossy1686@gmail.com>
2019-06-12 11:30:11 +0200

    added ability to like and unlike posts routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -89,4 +89,46 @@ router.delete('/:id', auth, async (req, res) => {
 	}
 });
 
+// Add likes to a comment
+router.put('/like/:id', auth, async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+
+		if (post.likes.filter((like) => like.user.toString() === req.user.id).length > 0) {
+			return res.status(400).json({ msg: 'Post already liked' });
+		}
+
+		post.likes.unshift({ user: req.user.id });
+
+		await post.save();
+
+		res.json(post.likes);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server error');
+	}
+});
+
+// Remove a like to a comment
+router.put('/unlike/:id', auth, async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+
+		if (post.likes.filter((like) => like.user.toString() === req.user.id).length === 0) {
+			return res.status(400).json({ msg: 'Post has not yet been liked' });
+		}
+
+		const removeIndex = post.likes.map((like) => like.user.toString()).indexOf(req.user.id);
+
+		post.likes.splice(removeIndex, 1);
+
+		await post.save();
+
+		res.json(post.likes);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server error');
+	}
+});
+
 module.exports = router;
