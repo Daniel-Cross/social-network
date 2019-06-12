0ba1cd621f4a9c620455ab686a31769408e8afb0
Daniel Cross <crossy1686@gmail.com>
2019-06-12 20:33:39 +0200

    added client react app

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import './App.css';
+
+const App = () => {
+	return (
+		<Fragment className="App">
+			<p>App</p>
+		</Fragment>
+	);
+};
+
+export default App;
