9ae82000d1885813995fa98435beb58eee9f0dc7
Daniel Cross <crossy1686@gmail.com>
2019-07-20 16:04:38 +0100

    private route and dashboard added

---
+++ client/src/components/dashboard/dashboard.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+const Dashboard = (props) => {
+	return (
+		<Fragment>
+			<h1>Hello</h1>
+		</Fragment>
+	);
+};
+
+Dashboard.propTypes = {};
+
+export default Dashboard;
